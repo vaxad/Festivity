c566f62f75135cbfabd6cb549ca1a9ccf836ab7b
unknown <dhairudu0711@gmail.com>
2023-06-03 16:05:25 +0530

    scv

--- components/Post.js
+++ components/Post.js
@@ -6,12 +6,13 @@ const Post= () => {
         return (
             <View style={styles.container}>
               <TouchableOpacity style={styles.buttonContainer} onPress={() => console.log('Button Pressed!')}>
-                <Image
+                <ImageBackground
                   source={require('./path/to/image.png')} // Replace with the path to your image
                   style={styles.buttonImage}
-                />
+                >
+                <Text style={styles.buttonText}>Click Me</Text></ImageBackground>
               </TouchableOpacity>
-              <Text style={styles.buttonText}>Click Me</Text>
+              
             </View>
     );
   };
