9949ca185bf256f1c4c544c37b0f5ed0fb95ce61
unknown <dhairudu0711@gmail.com>
2023-06-03 16:32:25 +0530

    vsfv

--- app/login/Login.js
+++ app/login/Login.js
@@ -1,32 +1,13 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import * as React from 'react'
-import { COLORS } from '../../constants'
-import styles from '../../styles/common.style'
-import { useNavigation } from 'expo-router'
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
-import AsyncStorage from '@react-native-async-storage/async-storage'
-// android 1042722321457-r6q29ikfsr2sit6aom2o5d5gt14e054c.apps.googleusercontent.com
-//ios 1042722321457-spvn3jkcgbg7n4te1fjiq061lgu62fds.apps.googleusercontent.com
-WebBrowser.maybeCompleteAuthSession();
+import { View, Text,SafeAreaView } from 'react-native'
+import React from 'react'
+import PhoneNumberInputExample from './PhoneNumberInputExample';
 
 const Login = () => {
-    const [user,setUser]=React.useState(null)
-    
-    const navigation=useNavigation();
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
-        <View style={{marginTop:90}}>
-        <View style={styles.container}>
-            <Text style={styles.welcomeMessage}>login</Text>
-            
-            <TouchableOpacity onPress={()=>{navigation.navigate('tabs')}}>
-                <Text style={styles.userName}>sign in</Text>
-            </TouchableOpacity>
-        </View>
-        </View>
+    <SafeAreaView>
+      <PhoneNumberInputExample />
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default Login
---
+++ components/Login.js
@@ -0,0 +1,33 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import { PhoneNumberInput } from 'react-native-phone-number-input';
+import Login from '../app/login/Login';
+
+const Login = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+  
+    const handlePhoneNumberChange = (number) => {
+      setPhoneNumber(number);
+    };
+  
+    const handleSubmit = () => {
+      // Handle form submission
+      console.log(phoneNumber);
+    };
+  
+    return (
+      <View>
+        <PhoneNumberInput
+          defaultValue={phoneNumber}
+          defaultCode="US" // Default country code
+          onChangeFormattedText={handlePhoneNumberChange}
+          withDarkTheme // Optional: If you want to use a dark theme
+        />
+        <Button title="Submit" onPress={handleSubmit} />
+      </View>
+    );
+  };
+  
+  export default Login;
