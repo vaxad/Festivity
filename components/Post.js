840b790e6724ec25cbf27e39a866c81d2118f139
vaxad <varadprabhu111@gmail.com>
2023-06-03 21:31:49 +0530

    KM

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,7 @@
 //import { View, Text } from 'react-native'
 import React from 'react'
 import {Text,TouchableOpacity,StyleSheet, ImageBackground,View} from 'react-native';
-import { SIZES } from '../constants';
+import { COLORS, FONT, SIZES } from '../constants';
 
 const Post= ({title,content,onPress}) => {
         return (
@@ -11,14 +11,18 @@ const Post= ({title,content,onPress}) => {
                   source={require('../assets/images/kemal.jpg')} // Replace with the path to your image
                   style={styles.buttonImage}
                   borderRadius={20}
+                  
                 >
                 <Text style={styles.buttonText}>{title}</Text>
-                <Text style={styles.buttonText}>{content}</Text>
+                <Text style={styles.buttonsText} numberOfLines={3}>{content}</Text>
                 </ImageBackground>
               </TouchableOpacity>
               
+              
+              
             
             </View>
+           
     );
   };
   
@@ -31,6 +35,21 @@ const Post= ({title,content,onPress}) => {
       justifyContent: 'center',
       alignItems: 'center',
     },
+    buttonText:{
+      
+        fontFamily: FONT.bold,
+      fontSize: SIZES.xLarge,
+      color: COLORS.white,
+      marginTop: -6,
+      shad
+      },
+      buttonsText:{
+        fontFamily: FONT.medium,
+        fontSize: SIZES.small,
+        color: COLORS.white,
+        
+      },
+    
     buttonContainer: {
       
       // width: 400,
@@ -40,15 +59,15 @@ const Post= ({title,content,onPress}) => {
       // // alignItems: 'center',
       // backgroundColor: 'grey',
       // padding: 20,
-      borderRadius: SIZES.medium,
+      
     },
     buttonImage: {
       
       left:-18,
       width: 325,
       height: 206,
-      top: 45,
-      paddingTop: 150,
+      top: -7,
+      paddingTop: 130,
       paddingLeft:10,
       
       
