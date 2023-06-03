b10f19b5400b361b4ebbc113a2d5385bbb5299c6
vaxad <varadprabhu111@gmail.com>
2023-06-03 18:16:50 +0530

    login start

--- app/login/Login.js
+++ app/login/Login.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { View, SafeAreaView, ScrollView, Text } from "react-native";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import Loader from "../../components/Loader";
+import { useState } from "react";
+import { COLORS } from "../../constants";
+
 
 const Login = () => {
+  const [phone,setPhone]=useState('')
+  const [code,setCode]=useState('')
+  const [loading,setLoading]=useState(false);
+  const [isDisabled,setisDisabled]=useState('')
+  const [isDisabled2,setisDisabled2]=useState('')
+
+
+
   return (
-    <SafeAreaView>
-      
+    <SafeAreaView style={{ backgroundColor: COLORS.white, flex: 1 }}>
+      <Loader visible={loading} />
+      <ScrollView
+        contentContainerStyle={{ paddingTop: 50, paddingHorizontal: 20 }}>
+        
+        <Text style={{ color: COLORS.primary, fontSize: 40, fontWeight: 'bold' }}>
+          Login using OTP
+        </Text>
+
+        <View style={{ marginVertical: 20 }}>
+
+          <Input
+            keyboardType="numeric"
+            onChangeText={setPhone}
+            value={phone}
+            onFocus={() => {}}
+            iconName="phone-outline"
+            label="Phone Number"
+            placeholder="Enter your phone no"
+            //error={errors.phone}
+          />
+          <Button title="Send OTP" onPress={()=>{}}  />
+          <Input
+            onChangeText={setCode}
+            value={code}
+            iconName="lock-outline"
+            keyboardType="numeric"
+            label="OTP"
+            placeholder="Enter OTP"
+          />
+          <Button title="Verify OTP" onPress={()=>{}}  />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {TouchableOpacity, Text} from 'react-native';
+import {COLORS, SIZES} from '../constants';
+const Button = ({title, onPress = () => {},disabled}) => {
+  let color=COLORS.primary;
+  if(disabled){
+     color=COLORS.gray;
+  }else{
+    color=COLORS.primary;
+  }
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.7}
+      disabled={disabled}
+      style={{
+        height: 55,
+        width: '100%',
+        backgroundColor: color,
+    borderRadius:SIZES.large/0.5,
+        marginVertical: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text style={{color: COLORS.white, fontWeight: 'bold', fontSize: 18}}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
