887f6bee6b27e5a3fe5f136ddc41fb316a3478d7
unknown <dhairudu0711@gmail.com>
2023-06-03 15:12:39 +0530

    dsfbsl

--- app/(tabs)/Posts.js
+++ app/(tabs)/Posts.js
@@ -4,21 +4,36 @@ import styles from '../../styles/common.style'
 import { COLORS } from '../../constants'
 import Post from '../../components/common/cards/Post'
 
-const Posts = () => {
-  return (
-    <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
-      <View style={{marginTop:90}}/>
-      <View style={styles.container}>
-      <Text style={styles.welcomeMessage}>Posts</Text>
-      <Text style={styles.userName}>here</Text>
-      <View>
-        <Post title="Rudra" content="hjtjhhhgj" onPress={()=>{}}/>
-      </View>
-      </View>
-    </SafeAreaView>
-  )
-}
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
+const App = () => {
+  const [imageUri, setImageUri] = useState(null);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log('Error selecting image:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.buttonContainer} onPress={selectImage}>
+        {imageUri ? (
+          <Image source={{ uri: imageUri }} style={styles.buttonImage} />
+        ) : (
+          <Image source={require('./path/to/default/image.png')} style={styles.buttonImage} />
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 
 export default Posts
