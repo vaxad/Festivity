353429953be1efdbfc0e10341ff4104552abbb70
unknown <dhairudu0711@gmail.com>
2023-06-03 15:50:57 +0530

    dcv

---
+++ components/Post.js
@@ -0,0 +1,41 @@
+//import { View, Text } from 'react-native'
+import React from 'react'
+import {View,Text,TouchableOpacity,StyleSheet} from 'react-native';
+
+const Post= () => {
+        return (
+            <View style={styles.container}>
+              <TouchableOpacity style={styles.buttonContainer} onPress={() => console.log('Button Pressed!')}>
+                <Image
+                  source={require('./path/to/image.png')} // Replace with the path to your image
+                  style={styles.buttonImage}
+                />
+                <Text style={styles.buttonText}>Click Me</Text>
+              </TouchableOpacity>
+            </View>
+    );
+  };
+  
+    
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonContainer: {
+      width: 200,
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightgray',
+    },
+    buttonImage: {
+      width: 20,
+      height: 20,
+    },
+  });
+  
+
+export default Post
