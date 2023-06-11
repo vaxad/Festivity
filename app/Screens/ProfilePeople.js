cd7e62085279a0c5589cbba24338a4cfc6a66f37
unknown <dhairudu0711@gmail.com>
2023-06-11 21:17:51 +0530

    vdfv

--- app/Screens/ProfilePeople.js
+++ app/Screens/ProfilePeople.js
@@ -1,154 +1,110 @@
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native'
-import React,{useEffect} from 'react'
-import styles from '../../styles/common.style'
-import { COLORS ,SIZES} from '../../constants'
-import { Image } from 'react-native'
-import Button from '../../components/Button'
-import { useDispatch, useSelector } from 'react-redux'
-import { logout, loadUser } from '../../redux/action'
-import { useNavigation } from 'expo-router'
-import { ScrollView } from 'react-native-gesture-handler'
-
+import { View, Text, SafeAreaView, StyleSheet, Image, useWindowDimensions } from 'react-native';
+import React, { useEffect } from 'react';
+import styles from '../../styles/common.style';
+import { COLORS, SIZES } from '../../constants';
+import Button from '../../components/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, loadUser } from '../../redux/action';
+import { useNavigation } from 'expo-router';
+import { ScrollView } from 'react-native-gesture-handler';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
 const ProfilePeople = () => {
-    const { token } = useSelector(state => state.auth)
-  const navigation=useNavigation()
-  const dispatch=useDispatch();
+  const { token } = useSelector(state => state.auth);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
+
   useEffect(() => {
     dispatch(loadUser(token));
   }, []);
-  var { user } = useSelector(state => state.auth)
-  if(!user){
-    navigation.navigate('login2')
-    }
+
+  var { user } = useSelector(state => state.auth);
+  if (!user) {
+    navigation.navigate('login2');
+  }
+
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
-    <View style={{marginTop:90}}/>
-    <ScrollView>
-      <View style={styles.container}>
-      <Text style={styles.welcomeMessage}>Profile</Text>
-      <Image 
-      source={require('../../assets/images/kemal.jpg')}  
-      style={{width: 200, height: 200, borderRadius: 400/ 2,alignSelf: 'center'}} 
-      />
-      <Text style={styles.userName}>{user?user.name:""}</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <View style={{ marginTop: 90 }} />
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.welcomeMessage}>Profile</Text>
+          <Image
+            source={require('../../assets/images/kemal.jpg')}
+            style={{ width: 200, height: 200, borderRadius: 400 / 2, alignSelf: 'center' }}
+          />
+          <Text style={styles.userName}>{user ? user.name : ''}</Text>
 
-      <View
-        style={[
-          style.inputContainer,
-          {
-            width:110,
-            top:10,
-            borderColor:COLORS.black,
-            alignItems: 'center',
-            
-          },
-        ]}>
-          <Text>{user?user.phone:""}</Text>
+          <View
+            style={[
+              styles.inputContainer,
+              {
+                width: 110,
+                top: 10,
+                borderColor: COLORS.black,
+                alignItems: 'center',
+              },
+            ]}
+          >
+            <Text>{user ? user.phone : ''}</Text>
+          </View>
+
+          <View
+            style={[
+              styles.inputContainer,
+              {
+                width: 200,
+                top: 22,
+                borderColor: COLORS.black,
+                alignItems: 'center',
+              },
+            ]}
+          >
+            <Text>Mumbai, Maharashtra, India.</Text>
+          </View>
+
+          <View
+            style={[
+              styles.inputContainer,
+              {
+                width: 340,
+                top: 81,
+                left: 7,
+                borderColor: COLORS.black,
+                alignItems: 'center',
+              },
+            ]}
+          >
+            {/* Content for additional views */}
+          </View>
+
+          <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            initialLayout={{ width: layout.width }}
+          />
+
         </View>
-
-        <View
-        style={[
-          
-          style.inputContainer,
-          {
-            width:200,
-            top:22,
-            borderColor:COLORS.black,
-            alignItems: 'center',
-          },
-        ]}>
-          <Text>Mumbai, Maharashtra, India.</Text>
-        </View>
-        
-        <View
-        style={[
-          style.inputContainer,
-          {
-            width:340,
-           top:81,
-           left:7,
-
-            borderColor:COLORS.black,
-            alignItems: 'center',
-            
-            
-          },
-        ]}>
-        {/* <View
-        style={[
-          style.inputContainer,
-          {
-            borderColor:COLORS.black,
-            alignItems: 'center',
-          },
-        ]}><Text>Interest1</Text></View>
-
-
-        <View
-        style={[
-          style.inputContainer,
-          {
-            borderColor:COLORS.black,
-            alignItems: 'center',
-          },
-        ]}><Text>Interest2</Text></View> */}
-
-        {/* <TouchableOpacity>
-        <View
-        style={[
-          style.inputContainer,
-          {
-            borderColor:COLORS.black,
-            alignItems: 'center',
-          },
-        ]}><Text>+</Text></View>
-        </TouchableOpacity> */}
-        
-        </View>
-        
-        <View style={{ flexDirection: 'row' }}>
-      <TouchableOpacity
-        style={{ flex: 1, backgroundColor: 'black', margin: 10, padding: 10 }}
-        onPress={() => console.log('Touchable 1 pressed')}
-      >
-        <Text style={{ color: 'white', textAlign: 'center' }}>Review</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={{ flex: 1, backgroundColor: 'black', margin: 10, padding: 10}}
-        onPress={() => console.log('Touchable 2 pressed')}
-      >
-        <Text style={{ color: 'white', textAlign: 'center' }}>Posts</Text>
-      </TouchableOpacity>
-    </View>
-      
-        
-      </View>
       </ScrollView>
     </SafeAreaView>
-  )
-} 
-const style = StyleSheet.create({
-  label: {
-    marginVertical: 5,
-    fontSize: 14,
-    color: COLORS.gray,
-    
-  },
-  // Button:{
-  //  width:340,
-  // },
- 
-  inputContainer: {
-    
-    height: 50,
-    backgroundColor: COLORS.light,
-    borderRadius:SIZES.large/0.5,
-    flexDirection: 'row',
-    paddingHorizontal: 15,
-    borderWidth: 0.5,
-  },
+  );
+};
+
+const FirstRoute = () => <View style={{ flex: 1, backgroundColor: '#ff4081' }} />;
+
+const SecondRoute = () => <View style={{ flex: 1, backgroundColor: '#673ab7' }} />;
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
 });
 
 export default ProfilePeople
